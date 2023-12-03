0a41c03a5cea041fb4aeee27a411deaa58486b72
Max Tutelyan <tutel.ws.dev@gmail.com>
2023-12-04 00:19:19 +0300

    flashlight fix

--- Assets/Scripts/Components/CharacterControllers/classes/Flashlight.cs
+++ Assets/Scripts/Components/CharacterControllers/classes/Flashlight.cs
@@ -11,30 +11,41 @@ namespace Components.CharacterControllers.classes
         private StarterAssetsInputs _input;
         private bool _isTurnedOn;
 
+        private bool _subscribed;
+
         public void Start()
         {
+            if (_subscribed)
+                return;
+            
             if (_input == null)
             {
                 var inputObject = GameObject.FindGameObjectWithTag("PlayerInput");
                 _input = inputObject.GetComponent<StarterAssetsInputs>();
             }
-           
+
+            _subscribed = true;
             _input.clickFEvent += doOnInputPressed;
         }
 
         public void OnEnable()
         {
+            if (_subscribed)
+                return;
+            
             if (_input == null)
             {
                 var inputObject = GameObject.FindGameObjectWithTag("PlayerInput");
                 _input = inputObject.GetComponent<StarterAssetsInputs>();
             }
-            
+
+            _subscribed = true;
             _input.clickFEvent += doOnInputPressed;
         }
 
         public void OnDisable()
         {
+            _subscribed = false;
             _input.clickFEvent -= doOnInputPressed;
         }
 
